d336db370c7062369fc2508eb6acf6d6cd3644da
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-08 15:37:58 -0700

    Complete Callback Review tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,14 +5,35 @@
 var fs = require('fs');
 var request = require('request');
 
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      callback(err, fileData);
+    } else {
+      //need the first line
+      //need to transform the data we got into a string
+      var firstLine = fileData.toString();
+      //now need first line, we can split the string into an array by line and then grab the first line
+      firstLine = firstLine.split('\n');
+      callback(err, firstLine[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  //need to make a get request and then use the callback to return the request.statuscod
+  request(url, (err, response) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
